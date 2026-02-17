f29895cfffd5578f50b79b35e380d0eae6e0e72a
abhiram kosuru <abhiramkosuru967@gmail.com>
2026-02-17 15:13:28 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { Pool } = require("pg");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+const createTable = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS tasks (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      completed BOOLEAN DEFAULT false,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
+};
+
+createTable();
+
+
+app.get("/", (req, res) => {
+  res.send("DevTask API Running 🚀");
+});
+
+app.get("/tasks", async (req, res) => {
+  const result = await pool.query("SELECT * FROM tasks");
+  res.json(result.rows);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { title } = req.body;
+  const result = await pool.query(
+    "INSERT INTO tasks (title) VALUES ($1) RETURNING *",
+    [title]
+  );
+  res.json(result.rows[0]);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
